31de93e545bb16923045386b2e45ab7edd442a33
deveb1479@gmail.com <deveb1479@gmail.com>
2024-08-14 16:29:58 +0900

    edit now

--- BlueTelephone/MainForm.Designer.cs
+++ BlueTelephone/MainForm.Designer.cs
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(751, 612);
+            Name = "MainForm";
+            Text = "Blue Telephone";
+            ResumeLayout(false);
         }
 
         #endregion
--- BlueTelephone/MainForm.cs
+++ BlueTelephone/MainForm.cs
@@ -6,9 +6,12 @@ namespace BlueTelephone
 {
     public partial class MainForm : Form
     {
+        TextBox
+
         public MainForm()
         {
             InitializeComponent();
+            Designer();
 
             TcpListener listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 12000);
             listener.Start();
@@ -21,6 +24,23 @@ namespace BlueTelephone
             });
 
             listener.AcceptTcpClient();
+        }
+
+        public void Designer()
+        {
+            TableLayoutPanel panel = new TableLayoutPanel()
+            {
+                Parent = this,
+                Visible = true,
+            };
+
+            panel.RowStyles.Add(new RowStyle() { Height = 50, SizeType = SizeType.Absolute });
+            panel.RowStyles.Add(new RowStyle() { Height = 50, SizeType = SizeType.Absolute });
+            panel.RowStyles.Add(new RowStyle() { Height = 1, SizeType = SizeType.Percent });
+
+            panel.ColumnStyles.Add(new ColumnStyle() { Width = 1, SizeType = SizeType.Percent });
+            panel.ColumnStyles.Add(new ColumnStyle() { Width = 1, SizeType = SizeType.Percent });
+            panel.ColumnStyles.Add(new ColumnStyle() { Width = 150, SizeType = SizeType.Absolute });
         }
     }
 }
