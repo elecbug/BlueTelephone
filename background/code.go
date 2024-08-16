0d866202c09fa0f5a3086bbde90f00049cfbe529
elecbug <deveb1479@gmail.com>
2024-08-16 21:46:03 +0900

    add commnet in go part(kr)

--- background/code.go
+++ background/code.go
@@ -1,14 +1,24 @@
 package main
 
 const (
-	PanicError  = -1
+	// 이건 못살리는 에러(송)
+	PanicError = -1
+	// 살릴만한 에러(송)
 	DeniedError = 0
-	Success     = 1
-	CreateHost  = 2
-	FoundPeer   = 3
-	RemovePeer  = 4
-	JoinGossip  = 5
-	ExitGossip  = 6
-	Publish     = 7
-	GotGossip   = 8
+	// 백그라운드로 전달한 명령 성공(송)
+	Success = 1
+	// 호스트가 생성되었음(송)
+	CreateHost = 2
+	// 주위 피어를 찾았음(송)
+	FoundPeer = 3
+	// 주위 피어가 사라짐(송)
+	RemovePeer = 4
+	// 채팅방에 참여하고 싶음(수)
+	JoinGossip = 5
+	// 채팅방에서 나가고 싶음(수)
+	ExitGossip = 6
+	// 메시지를 게시하고 싶음(수)
+	Publish = 7
+	// 메시지가 왔음(송)
+	GotGossip = 8
 )
--- background/main.go
+++ background/main.go
@@ -26,29 +26,40 @@ import (
 	"github.com/multiformats/go-multiaddr"
 )
 
+// 주위 피어를 찾았을 때 닉네임을 전달하는 프로토콜
 const NameExchangeProtocol = "/blue-telephone/name-exchange/1.0.0"
-const TlsProtocol = "/blue-telephone/tls/1.0.0"
 
+// 메인 함수
 func main() {
+	// 플래그 전달 받음
 	group, name, port := CreateFlag()
 
+	// 폼으로 다이얼
+	// 이 시점에서 폼은 리스너가 준비됨
 	conn, err := net.Dial("tcp4", fmt.Sprintf("127.0.0.1:%d", port))
 
+	// 즉 아래는 폼이 문제가 없다면 발생하지 않음
 	if err != nil {
 		log.Fatalln(err)
 	}
 
+	// 메인 context 형성
 	ctx := context.Background()
+	// 발견한 친구 목록
 	friends := []PeerName{}
+	// 구독한 토픽 목록
 	topics := []Topic{}
 
+	// Gossip 형성
 	_, gossip := CreateHostAndExchangeInfo(ctx, group, name, friends, conn)
 
+	// 무한 대기하며 폼과 통신
 	for {
 		buf := make([]byte, 1024)
 
 		_, err := io.ReadFull(conn, buf)
 
+		// 메시지 못읽겠으면 오류 전달
 		if err != nil {
 			WritePacket(conn, DeniedError, []string{err.Error()})
 			log.Println(err)
@@ -59,13 +70,16 @@ func main() {
 
 		err = json.Unmarshal([]byte(strings.TrimRight(string(buf), "\x00")), &packet)
 
+		// 받은 패킷이 json으로 안풀리면 오류 전달
 		if err != nil {
 			WritePacket(conn, DeniedError, []string{err.Error()})
 			log.Println(err)
 			continue
 		}
 
+		// 메시지 코드에 따라 동작
 		switch packet.MsgCode {
+		// Gossip 참여 요청시 참여 및 구독 처리
 		case JoinGossip:
 			topic, err := gossip.Join(packet.Msg[0])
 
@@ -82,8 +96,10 @@ func main() {
 				continue
 			}
 
+			// 해당 토픽을 위한 자식 context 형성
 			topicCtx, cancelCtx := context.WithCancel(ctx)
 
+			// 토픽 목록에 추가
 			topics = append(topics, Topic{
 				topic:  topic,
 				sub:    sub,
@@ -91,10 +107,15 @@ func main() {
 				cancel: cancelCtx,
 			})
 
+			// 해당 토픽만을 위한 별도의 스레드 동작
+			// 토픽 취소할 때 함께 소멸
 			go func() {
 				for {
+					// 만약 메시지를 받으면 폼으로 전달
 					msg, err := sub.Next(topicCtx)
 
+					// 실패시 오류 핸들링
+					// 이긴 한데, 그 오류가 context canceled 즉, 토픽 구독 취소라면 Success 코드로 전송
 					if err != nil {
 						if err.Error() != "context canceled" {
 							WritePacket(conn, DeniedError, []string{err.Error()})
@@ -112,48 +133,67 @@ func main() {
 				}
 			}()
 
+			// 토픽 참여 성공 메시지 전달
 			WritePacket(conn, Success, []string{"Success joins topic"})
 			log.Println("Success joins topic")
 
+		// 토픽에서 나가고 싶다면
 		case ExitGossip:
+			// 토픽을 목록에서 찾아서 이탈
 			for i, v := range topics {
 				if v.topic.String() == packet.Msg[0] {
 					v.cancel()
 					v.sub.Cancel()
 					err = v.topic.Close()
 
+					// 이탈 실패시 발생
+					// 아직까지 발생한적 없음
 					if err != nil {
 						WritePacket(conn, DeniedError, []string{err.Error()})
 						log.Println(err)
 						continue
 					}
 
+					// 토픽 목록 정리
 					topics = append(topics[:i], topics[i+1:]...)
 
+					// 성공했다고 전달
 					WritePacket(conn, Success, []string{"Success exits topic"})
 					log.Println("Success exits topic")
+
+					break
 				}
 			}
 
+		// 메시지 전달 요청 시
 		case Publish:
+			// 토픽에서 찾아보고
 			for _, v := range topics {
+				// 해당 토픽에서
 				if v.topic.String() == packet.Msg[0] {
+					// 퍼블리시하고
 					err = v.topic.Publish(ctx, []byte(packet.Msg[1]))
 
+					// 실패 알림
+					// 이것도 발생한 적 없음
 					if err != nil {
 						WritePacket(conn, DeniedError, []string{err.Error()})
 						log.Println(err)
 						continue
 					}
 
+					// 성공했다고 전달
 					WritePacket(conn, Success, []string{"Success publish topic"})
 					log.Println("Success publish topic")
+
+					break
 				}
 			}
 		}
 	}
 }
 
+// 실행 폼으로부터 플래그를 받는 함수, (그룹, 이름, 포트) 순서
 func CreateFlag() (string, string, int) {
 	group := flag.String("group", "default", "group(mdns rendezvous)")
 	name := flag.String("name", fmt.Sprintf("BT-%d", rand.Int()), "user nick name")
@@ -164,7 +204,9 @@ func CreateFlag() (string, string, int) {
 	return *group, *name, *port
 }
 
+// 호스트를 생성하고, 주위 피어를 찾거나 닉네임을 전파하는 역할을 하는 함수
 func CreateHostAndExchangeInfo(ctx context.Context, rendezvous string, name string, friends []PeerName, conn net.Conn) (host.Host, *pubsub.PubSub) {
+	// 호스트 생성
 	host, err := libp2p.New(
 		libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/0"),
 		libp2p.Security(noise.ID, noise.New),
@@ -176,6 +218,7 @@ func CreateHostAndExchangeInfo(ctx context.Context, rendezvous string, name stri
 		WritePacket(conn, PanicError, []string{err.Error()})
 		log.Fatalln(err)
 	} else {
+		// 에러가 없을 시 주소 정보를 폼으로 전송
 		addrs := make([]string, len(host.Addrs()))
 
 		for i, v := range host.Addrs() {
@@ -186,6 +229,7 @@ func CreateHostAndExchangeInfo(ctx context.Context, rendezvous string, name stri
 		log.Println("Self:", host.Addrs(), host.ID())
 	}
 
+	// Gossipsub 생성
 	ps, err := pubsub.NewGossipSub(ctx, host)
 
 	if err != nil {
@@ -193,9 +237,13 @@ func CreateHostAndExchangeInfo(ctx context.Context, rendezvous string, name stri
 		log.Fatalln(err)
 	}
 
+	// MDNS 프로토콜 설정
 	peerChan := InitMDNS(host, rendezvous, conn)
 
+	// 스레드 분할 시작
+	// 피어 찾기 + 피어에게 이름 알려주기
 	go func() {
+		// 이름 교환 프로토콜에서 메시지를 받을 시 해당 피어의 이름 + 정보를 폼으로 전달
 		host.SetStreamHandler(protocol.ID(NameExchangeProtocol), func(stream network.Stream) {
 			buf := make([]byte, 1024)
 			stream.Read(buf)
@@ -214,10 +262,15 @@ func CreateHostAndExchangeInfo(ctx context.Context, rendezvous string, name stri
 			log.Println("Add:", stream.Conn().RemoteMultiaddr().String(), stream.Conn().RemotePeer(), string(buf))
 		})
 
+		// 피어를 찾는다면
 		for {
 			peer := <-peerChan
 
+			// 피어를 스토어에 저장
+			// ** 절대 직접 커넥션하면 안됨 - noise handshake에서 오류가 발생 **
 			host.Peerstore().AddAddrs(peer.ID, peer.Addrs, 1*time.Hour)
+
+			// 스트림을 형성하고 자신의 이름을 상대에게 전송
 			stream, err := host.NewStream(ctx, peer.ID, protocol.ID(NameExchangeProtocol))
 
 			if err != nil {
@@ -244,10 +297,13 @@ func CreateHostAndExchangeInfo(ctx context.Context, rendezvous string, name stri
 		}
 	}()
 
+	// 피어 삭제 스레드
 	go func() {
 		for {
+			// 10초마다
 			time.Sleep(10 * time.Second)
 
+			// 상ㄷ애가 대답이 없다면 친구 목록에서 제거하고 삭제되었음을 폼으로 전송
 			for i, v := range friends {
 				err := host.Connect(ctx, v.info)
 
@@ -266,6 +322,7 @@ func CreateHostAndExchangeInfo(ctx context.Context, rendezvous string, name stri
 	return host, ps
 }
 
+// 폼으로 패킷을 전달하는 함수
 func WritePacket(conn net.Conn, msgCode int, msg []string) {
 	packet := &Packet{
 		TS:      time.Now().String(),
@@ -282,43 +339,54 @@ func WritePacket(conn net.Conn, msgCode int, msg []string) {
 	buf := make([]byte, 1024)
 	copy(buf, []byte(string(json)))
 
-	if len(buf) > 1024 {
-		WritePacket(conn, DeniedError, []string{"packet size over 1024"})
-	} else {
-		_, err = conn.Write(buf)
+	_, err = conn.Write(buf)
 
-		if err != nil {
-			log.Fatalln(err)
-		}
+	if err != nil {
+		log.Fatalln(err)
 	}
 }
 
+// 피어 이름 구조체
 type PeerName struct {
+	// libp2p에서의 정보
 	info peer.AddrInfo
+	// 피어가 자칭한 닉네임
 	name string
 }
 
+// MDNS용 구조체
 type DiscoveryNotifee struct {
 	PeerChan chan peer.AddrInfo
 }
 
+// 패킷 구조체
 type Packet struct {
-	TS      string
+	// 타임스탬프
+	TS string
+	// 메시지 코드
 	MsgCode int
-	Msg     []string
+	// 메시지
+	Msg []string
 }
 
+// 토픽 구조체
 type Topic struct {
-	topic  *pubsub.Topic
-	sub    *pubsub.Subscription
-	ctx    context.Context
+	// Gossipsub에서의 토픽
+	topic *pubsub.Topic
+	// Gossipsub에서의 구독 정보
+	sub *pubsub.Subscription
+	// 해당 토픽을 제어하기 위한 context
+	ctx context.Context
+	// 해당 토픽 context의 취소 명령
 	cancel context.CancelFunc
 }
 
+// MDNS용 피어 발견 제어 핸들러
 func (n *DiscoveryNotifee) HandlePeerFound(pi peer.AddrInfo) {
 	n.PeerChan <- pi
 }
 
+// MDNS 형성
 func InitMDNS(peerhost host.Host, rendezvous string, conn net.Conn) chan peer.AddrInfo {
 	n := &DiscoveryNotifee{}
 	n.PeerChan = make(chan peer.AddrInfo)
